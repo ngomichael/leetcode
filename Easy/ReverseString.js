bc52ee11bb0f7130c95b8e46e1c17e2f8b6fb602
ngomichael <michael_ngo@live.com>
2018-09-19 12:29:55 -0700

    reverseString solution

---
+++ Easy/ReverseString.js
@@ -0,0 +1,27 @@
+// Write a function that takes a string as input and returns the string reversed.
+
+// Example 1:
+// Input: "hello"
+// Output: "olleh"
+
+// Example 2:
+// Input: "A man, a plan, a canal: Panama"
+// Output: "amanaP :lanac a ,nalp a ,nam A"
+
+//Runtime - O(n)
+function reverse(string) {
+  return [...string].reverse().join('');
+}
+
+//Runtime - O(n)
+function reverse(string) {
+  if (typeof string !== 'string') {
+    return -1;
+  }
+
+  let reversed = '';
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversed += string.charAt(i);
+  }
+  return reversed;
+}
